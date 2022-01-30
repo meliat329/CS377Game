c27e5ed2889b1d3ab3fe04d48445935fad65498e
aleonne <81379981+aleonne@users.noreply.github.com>
2022-01-30 13:26:56 -0600

    test scene

--- Xarzirath/Assets/Code/HealthKeeper.cs
+++ Xarzirath/Assets/Code/HealthKeeper.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class HealthKeeper : MonoBehaviour
 {
-    //should hold two rectangles, one of which will scale
 
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     //h is new heath, can be passed in by other parts of game
     public static void UpdateHealth(int h)
     {
-    
+
     }
 }
